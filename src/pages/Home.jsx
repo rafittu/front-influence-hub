3a34e9acc3906faca11f2dd7590784e05c2f8a9e
rafittu <rafittu@outlook.com>
2024-09-01 16:04:43 -0300

    feat: email input to login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Homepage() {
+  const [email, setEmail] = useState('');
+
+  const handleChange = (event) => event;
+
+  const handleSubmit = async (event) => event;
+
   return (
-    <h1>Influence Hub</h1>
+    <main>
+      <header>
+        <h1>Influence Hub</h1>
+      </header>
+
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div className="inputs-container">
+            <label htmlFor="email">
+              <input
+                name="email"
+                id="login"
+                value={email}
+                onChange={handleChange}
+                type="email"
+                required
+                placeholder="e-mail"
+              />
+            </label>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 }
 
