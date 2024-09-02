76f111943ac915699bb4f0c06be0201ea252e972
rafittu <rafittu@outlook.com>
2024-09-02 20:04:14 -0300

    feat: get all influencers and mount page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAdmin } from '../contexts/AdminContext';
+import { getAllInfluencersApi } from '../api/InfluencerAPI';
 
 function Dashboard() {
   const { adminData } = useAdmin();
+  const [influencers, setInfluencers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   console.log(adminData);
+  console.log(isLoading);
+  console.log(influencers);
+  console.log(error);
+
+  useEffect(() => {
+    const fetchInfluencers = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const
+          accessToken = localStorage.getItem('metropole4');
+
+        const data = await getAllInfluencersApi(accessToken);
+        setInfluencers(data);
+      } catch (err) {
+        setError('Erro ao carregar influenciadores');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchInfluencers();
+  }, []);
 
   return (
     <main>
