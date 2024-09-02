a5e07de5dd31594c1ad25796cf4322eb17a42f0c
rafittu <rafittu@outlook.com>
2024-09-02 12:16:20 -0300

    style: initial homepage css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
 
+import '../styles/Home.css';
+
 function Homepage() {
   return (
-    <main>
-      <header>
-        <h1>Influence Hub</h1>
+    <main id="homepage-main">
+      <header id="brand-logo">
+        <img src="https://metropole4.s3.amazonaws.com/logo.png" alt="Influence Hub Logo" />
       </header>
 
       <section>
