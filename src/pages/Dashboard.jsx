1984c0f3e245c1e4087db74d23b5e1036973d351
rafittu <rafittu@outlook.com>
2024-09-03 14:26:33 -0300

    feat: create influencer filter button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getAllInfluencersApi } from '../api/InfluencerAPI';
+import { getAllInfluencersApi, getInfluencersByFilterApi } from '../api/InfluencerAPI';
 import InfluencersTable from '../components/InfluencersTable';
 import NavigationBar from '../components/NavigationBar';
+import FilterModal from '../components/FilterModal';
 
 import '../styles/Dashboard.css';
 
 function Dashboard() {
   const [influencers, setInfluencers] = useState([]);
+  const [filters, setFilters] = useState({});
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -18,7 +21,14 @@ function Dashboard() {
 
       try {
         const accessToken = localStorage.getItem('metropole4');
-        const data = await getAllInfluencersApi(accessToken);
+        let data;
+
+        if (Object.keys(filters).length > 0) {
+          data = await getInfluencersByFilterApi(accessToken, filters);
+        } else {
+          data = await getAllInfluencersApi(accessToken);
+        }
+
         setInfluencers(data);
       } catch (err) {
         setError('Erro ao carregar influenciadores');
@@ -28,7 +38,7 @@ function Dashboard() {
     };
 
     fetchInfluencers();
-  }, []);
+  }, [filters]);
 
   return (
     <main id="dashboard-main">
@@ -37,27 +47,39 @@ function Dashboard() {
       </header>
 
       <section id="dash-section">
+        <div id="table-header">
+          <h2 id="dash-h2">
+            Listagem de
+            <span id="influenciador-h2"> Influenciadores</span>
+          </h2>
+
+          <span id="table-buttons">
+            <button type="button" onClick={() => setIsFilterModalOpen(true)}>
+              Filtrar
+            </button>
+
+            <Link to="/influencer/new">
+              <button type="button">Novo</button>
+            </Link>
+          </span>
+        </div>
+
         {!isLoading && (
           influencers.length > 0 ? (
-            <>
-              <div id="table-header">
-                <h2 id="dash-h2">
-                  Listagem de
-                  <span id="influenciador-h2"> Influenciadores</span>
-                </h2>
-
-                <Link to="/influencer/new">
-                  <button type="button">Novo</button>
-                </Link>
-              </div>
-
-              <InfluencersTable influencers={influencers} />
-            </>
+            <InfluencersTable influencers={influencers} />
           ) : (
             <p>{ error || 'Nenhum influenciador encontrado.'}</p>
           )
         )}
       </section>
+
+      {isFilterModalOpen && (
+      <FilterModal
+        setIsFilterModalOpen={setIsFilterModalOpen}
+        setFilters={setFilters}
+        filters={filters}
+      />
+      )}
     </main>
   );
 }
