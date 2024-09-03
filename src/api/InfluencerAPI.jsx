ee761c9be6731bbf628b9e43eab49abb428b9574
rafittu <rafittu@outlook.com>
2024-09-03 16:59:53 -0300

    feat: create influencer api function

--- src/api/InfluencerAPI.jsx
+++ src/api/InfluencerAPI.jsx
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 const baseUrl = process.env.REACT_APP_IH_BASE_API_URL || '';
 
+export const createInfluencerApi = async (data) => {
+  try {
+    const response = await axios.post(`${baseUrl}/influencer/create`, data);
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const getAllInfluencersApi = async (accessToken) => {
   try {
     const response = await axios.get(`${baseUrl}/influencer/all`, {
