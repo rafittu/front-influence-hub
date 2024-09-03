e0c6d7ca75bfc128673fb925676031e6086c5c42
rafittu <rafittu@outlook.com>
2024-09-03 11:00:18 -0300

    feat: create app navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAdmin } from '../contexts/AdminContext';
+
+import '../styles/NavigationBar.css';
 
 function NavigationBar() {
+  const navigate = useNavigate();
+  const { adminData } = useAdmin();
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem('metropole4');
+    navigate('/');
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>Influenciadores</li>
-        <li>Marcas</li>
-        <li>Gestão</li>
+    <nav className="navbar">
+      <div
+        className="admin-dropdown"
+        onMouseEnter={() => setShowDropdown(true)}
+        onMouseLeave={() => setShowDropdown(false)}
+      >
+        <button type="button" className="dropdown-button">{adminData.name}</button>
+        {showDropdown && (
+          <div className="dropdown-menu">
+            <button type="button" onClick={handleLogout}>Logout</button>
+          </div>
+        )}
+      </div>
+
+      <h1 id="nav-logo">Influence Hub</h1>
+
+      <ul className="links-list">
+        <Link to="/dashboard" id="dashboard-link">
+          <li>Influenciadores</li>
+        </Link>
+
+        <Link to="/brands" id="brands-link">
+          <li>Marcas</li>
+        </Link>
+
+        <Link to="/brand-influencers" id="brand-influencers-link">
+          <li>Gestão</li>
+        </Link>
       </ul>
     </nav>
   );
