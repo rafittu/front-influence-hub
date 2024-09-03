aebb38ccb2094ff743bec79468abce471adc02e6
rafittu <rafittu@outlook.com>
2024-09-03 17:44:07 -0300

    feat: handle create influencer inputs

--- src/pages/CreateInfluencer.jsx
+++ src/pages/CreateInfluencer.jsx
@@ -22,17 +22,42 @@ function CreateInfluencer() {
 
   console.log(setFormData);
 
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
+
+    if (type === 'checkbox') {
+      const updatedNiches = formData.niches.includes(value)
+        ? formData.niches.filter((niche) => niche !== value)
+        : [...formData.niches, value];
+
+      setFormData((prev) => ({
+        ...prev,
+        niches: updatedNiches,
+      }));
+    } else {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+
+    if (name === 'zipCode' && value.length === 8) {
+      fetchAddressByZipCode(value);
+    }
+  };
+
   return (
     <main id="create-influencer-main">
       <header>
         <NavigationBar />
       </header>
 
-      <section>
+      <section id="new-influence-section">
         <h1>Cadastrar Influencer</h1>
 
         <InfluencerForm
           formData={formData}
+          onChange={handleChange}
           niches={Categories}
         />
       </section>
