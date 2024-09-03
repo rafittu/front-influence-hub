280611646726e257b63492bb514f06bc8c13ae23
rafittu <rafittu@outlook.com>
2024-09-03 15:51:17 -0300

    feat: create new influencer page schema

---
+++ src/pages/CreateInfluencer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+import '../styles/CreateInfluencer/CreateInfluencer.css';
+
+function CreateInfluencer() {
+  return (
+    <main id="create-influencer-main">
+      <header>
+        <NavigationBar />
+      </header>
+    </main>
+  );
+}
+
+export default CreateInfluencer;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Dashboard from '../pages/Dashboard';
+import CreateInfluencer from '../pages/CreateInfluencer';
 
 function Router() {
   return (
     <Routes>
+      <Route exact path="/influencer/new" element={<CreateInfluencer />} />
       <Route exact path="/dashboard" element={<Dashboard />} />
       <Route exact path="/" element={<Home />} />
     </Routes>
