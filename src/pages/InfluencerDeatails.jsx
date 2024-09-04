461363aa0d1fae58e684b172fb8aa482d87bc620
rafittu <rafittu@outlook.com>
2024-09-04 09:03:17 -0300

    feat: get influencer data api call

--- src/pages/InfluencerDeatails.jsx
+++ src/pages/InfluencerDeatails.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
+import { getInfluencersByIdApi } from '../api/InfluencerAPI';
 
 function InfluencerDetails() {
+  const { id } = useParams();
+  const [influencer, setInfluencer] = useState(null);
+
+  useEffect(() => {
+    const fetchInfluencer = async () => {
+      const accessToken = localStorage.getItem('metropole4');
+      const response = await getInfluencersByIdApi(accessToken, id);
+
+      setInfluencer(response);
+    };
+
+    fetchInfluencer();
+  }, []);
+
+  console.log(influencer);
+
   return (
     <main>
       <header>
