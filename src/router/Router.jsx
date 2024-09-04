cb40380e33414ae1fae5023da5ce1df21b947eab
rafittu <rafittu@outlook.com>
2024-09-04 11:10:25 -0300

    feat: update influencer route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,12 +4,14 @@ import Home from '../pages/Home';
 import Dashboard from '../pages/Dashboard';
 import CreateInfluencer from '../pages/CreateInfluencer';
 import InfluencerDetails from '../pages/InfluencerDeatails';
+import UpdateInfluencer from '../pages/UpdateInfluencer';
 
 function Router() {
   return (
     <Routes>
       <Route exact path="/influencer/new" element={<CreateInfluencer />} />
       <Route exact path="/influencer/:id" element={<InfluencerDetails />} />
+      <Route exact path="/influencer/:id/edit" element={<UpdateInfluencer />} />
       <Route exact path="/dashboard" element={<Dashboard />} />
       <Route exact path="/" element={<Home />} />
     </Routes>
