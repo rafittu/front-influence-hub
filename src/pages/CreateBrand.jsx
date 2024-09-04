f5912a847db5fa5926320adb1e69ef5a2da939f2
rafittu <rafittu@outlook.com>
2024-09-04 17:15:35 -0300

    feat: create brand route

---
+++ src/pages/CreateBrand.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+function CreateBrand() {
+  return (
+    <main id="add-brand-main">
+      <header>
+        <NavigationBar />
+      </header>
+
+    </main>
+  );
+}
+
+export default CreateBrand;
