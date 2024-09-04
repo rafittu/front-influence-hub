2c60a02d745aa7dce4f521636d770e29081acd67
rafittu <rafittu@outlook.com>
2024-09-04 17:28:13 -0300

    feat: create brand form

--- src/pages/CreateBrand.jsx
+++ src/pages/CreateBrand.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavigationBar from '../components/NavigationBar';
+import BrandForm from '../components/BrandForm';
+import Categories from '../utils/CategoryOptions';
 
 function CreateBrand() {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    niches: [],
+  });
+
+  console.log(setFormData);
+
   return (
     <main id="add-brand-main">
       <header>
         <NavigationBar />
       </header>
 
+      <section id="new-brand-section">
+        <h1>Cadastrar Marca</h1>
+
+        <BrandForm
+          formData={formData}
+          // onChange={handleChange}
+          // onSubmit={handleSubmit}
+          niches={Categories}
+        />
+      </section>
     </main>
   );
 }
