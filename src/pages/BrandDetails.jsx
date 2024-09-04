782c42c4cbefc35da5a25f52f158777f93b1645f
rafittu <rafittu@outlook.com>
2024-09-04 18:05:41 -0300

    feat: brand details route

---
+++ src/pages/BrandDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+function BrandDetails() {
+  return (
+    <main id="brand-main">
+      <header>
+        <NavigationBar />
+      </header>
+    </main>
+  );
+}
+
+export default BrandDetails;
