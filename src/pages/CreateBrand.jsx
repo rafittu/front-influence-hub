fb6c226c8674d59f376e3135ee56bc6eb2dac47b
rafittu <rafittu@outlook.com>
2024-09-04 17:38:03 -0300

    feat: submit brand form and create it

--- src/pages/CreateBrand.jsx
+++ src/pages/CreateBrand.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
 import NavigationBar from '../components/NavigationBar';
 import BrandForm from '../components/BrandForm';
 import Categories from '../utils/CategoryOptions';
+import { createBrandApi } from '../api/BrandsAPI';
 
 function CreateBrand() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: '',
     description: '',
     niches: [],
   });
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     const { name, value, type } = e.target;
@@ -30,6 +36,34 @@ function CreateBrand() {
     }
   };
 
+  const createBrand = async () => {
+    const accessToken = localStorage.getItem('metropole4');
+    const response = await createBrandApi(accessToken, formData);
+
+    if (response.status === 409) {
+      setError('Nome da marca já em uso.');
+      return null;
+    }
+
+    if (response instanceof AxiosError) {
+      setError('Falha ao cadastrar marca');
+      return null;
+    }
+
+    return response;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setIsLoading(true);
+
+    const brandId = await createBrand();
+    if (brandId) { navigate(`/brand/${brandId.id}`); }
+
+    setIsLoading(false);
+  };
+
   return (
     <main id="add-brand-main">
       <header>
@@ -42,9 +76,12 @@ function CreateBrand() {
         <BrandForm
           formData={formData}
           onChange={handleChange}
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           niches={Categories}
         />
+
+        {error && <p>{error}</p>}
+        {isLoading && <p>Carregando...</p>}
       </section>
     </main>
   );
