2cbdaf933c13173ca41a1a1c862d2bc5add57e8a
rafittu <rafittu@outlook.com>
2024-09-04 17:01:01 -0300

    feat: create brand table

---
+++ src/components/Brands/BrandsTable.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function BrandsTable({ brands }) {
+  return (
+    <table className="brands-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Descrição</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {brands.map((brand) => (
+          <tr key={brand.id}>
+            <td>{brand.id}</td>
+            <td>
+              <Link to={`/brand/${brand.id}`}>{brand.name}</Link>
+            </td>
+            <td>
+              <Link to={`/brand/${brand.id}`}>{brand.description}</Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+BrandsTable.propTypes = {
+  brands: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default BrandsTable;
