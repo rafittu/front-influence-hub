6e94da86b82b0f0f83c6703cb669bb7affe13c53
rafittu <rafittu@outlook.com>
2024-09-04 08:47:11 -0300

    feat: influencer details page schema

---
+++ src/pages/InfluencerDeatails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+function InfluencerDetails() {
+  return (
+    <main>
+      <header>
+        <NavigationBar />
+      </header>
+    </main>
+  );
+}
+
+export default InfluencerDetails;
