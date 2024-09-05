5e1e7eff79b52e2904b3f55df0adba661e033966
rafittu <rafittu@outlook.com>
2024-09-04 21:01:05 -0300

    feat: update brand route

---
+++ src/pages/UpdateBrand.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+function UpdateBrand() {
+  return (
+    <main>
+      <header>
+        <NavigationBar />
+      </header>
+    </main>
+  );
+}
+
+export default UpdateBrand;
